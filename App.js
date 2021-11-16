7ab0afda78ed3ef3e27ef73e20a333175c10bcfd
laurahyvari <laura.hyvarinen@icloud.com>
2021-11-16 17:12:48 +0200

    tehtava valmis

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const key = 'asCAISlaHeYoROyx6ercvLsJu2a8wMYA';
+  const [location, setLocation] = useState({latitude:60.201373, longitude: 24.934041, latitudeDelta: 0.0322,longitudeDelta:0.0221,});
+ 
+
+
+  const getLocation = async () => {
+    console.log(address);
+    const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${address}`
+    
+    try {
+      const response = await fetch(url);
+      const region = await response.json();
+      console.log(region);
+      const lat = region.results[0].locations.latLng.lat;
+      const lng = region.results[0].locations.latLng.lng;
+
+
+      setLocation({latitude: lat, longitude: lng, latitudeDelta: 0.0322, longitudeDelta:0.0221});
+
+      console.log(location);
+    } catch(e) {
+      console.log('Error', e);
+    };
+  }
+  
+
+ 
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.map}>
+      <MapView 
+      style={{flex: 1}}
+      region={location}
+      >
+        <Marker
+          coordinate={{latitude: location.latitude, longitude: location.longitude}}
+          
+        />
+      </MapView>
+      </View>
+      <View style={styles.search}>
+        <TextInput style={{width: '100%', fontSize: 20, fontWeight: 'bold', }} placeholder="osoite" onChangeText={text => setAddress(text)}></TextInput>
+        <Button title='HAE' onPress={getLocation}/>
+      </View>
     </View>
+
   );
 }
 
@@ -18,4 +62,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    height: '85%',
+    width: '100%',
+  },
+  search: {
+    height: '15%',
+    width: '100%',
+  },
+
 });
